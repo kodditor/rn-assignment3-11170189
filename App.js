02c91ef224d9eb21f13bd8796684e18db1a2161c
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-05-30 13:51:20 +0000

    Added Images and constants

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View>
+          <Text>Hello, Devs</Text>
+          <Text></Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: flex,
+    flexDirection: 'column',
+    gap: '1.5rem',
   },
+  title: {
+    fontSize: '25px',
+    fontWeight: '700'
+  }
 });
---
+++ constants.js
@@ -0,0 +1,150 @@
+export const categories = {
+    '001': {
+        title: 'Study',
+        image: 'assets/images/study.png',
+        color: '#FF5733',
+        accentColor: '#FFC2B3'
+    },
+    '002': {
+        title: 'Cook',
+        image: 'assets/images/cook.png',
+        color: '#33B5FF',
+        accentColor: '#B3E5FF'
+    },
+    '003': {
+        title: 'Code',
+        image: 'assets/images/code.png',
+        color: '#8D33FF',
+        accentColor: '#D3B3FF'
+    },
+    '004': {
+        title: 'Exercise',
+        image: 'assets/images/exercise.png',
+        color: '#FF33A6',
+        accentColor: '#FFB3D9'
+    },
+    '005': {
+        title: 'Socialize',
+        image: 'assets/images/socialize.png',
+        color: '#33FF57',
+        accentColor: '#B3FFBF'
+    },
+    '006': {
+        title: 'Lectures',
+        image: 'assets/images/lectures.png',
+        color: '#FFC733',
+        accentColor: '#FFF0B3'
+    },
+    '007': {
+        title: 'Appointments',
+        image: 'assets/images/appointments.png',
+        color: '#336EFF',
+        accentColor: '#B3CCFF'
+    },
+    '008': {
+        title: 'Volunteering',
+        image: 'assets/images/volunteering.png',
+        color: '#33FFE0',
+        accentColor: '#B3FFF4'
+    },
+    '009': {
+        title: 'Cleaning/Laundry',
+        image: 'assets/images/cleaning.png',
+        color: '#FF9133',
+        accentColor: '#FFD1B3'
+    },
+    '010': {
+        title: 'Internship',
+        image: 'assets/images/work.png',
+        color: '#A6A6A6',
+        accentColor: '#E0E0E0'
+    },   
+}
+
+const tasks = [
+    {
+        category: '003',
+        title: 'DCIT 202 Assignment',
+        scheduledDate: '2024-06-04'
+    },
+    {
+        category: '005',
+        title: 'COMPSSA Event',
+        scheduledDate: '2024-06-10'
+    },
+    {
+        category: '004',
+        title: 'Play Tennis',
+        scheduledDate: '2024-06-13'
+    },
+    {
+        category: '002',
+        title: 'Make Lunch',
+        scheduledDate: '2024-06-17'
+    },
+    {
+        category: '007',
+        title: 'Dentist Appointment',
+        scheduledDate: '2024-06-23'
+    },
+    {
+        category: '006',
+        title: 'DCIT 208 Lecture',
+        scheduledDate: '2024-06-27'
+    },
+    {
+        category: '003',
+        title: 'Work on personal project',
+        scheduledDate: '2024-07-04'
+    },
+    {
+        category: '008',
+        title: 'Contribute to Zizi Foundation',
+        scheduledDate: '2024-07-11'
+    },
+    {
+        category: '001',
+        title: 'Research on platform-specific features',
+        scheduledDate: '2024-07-14'
+    },
+    {
+        category: '004',
+        title: 'Play football with the boys',
+        scheduledDate: '2024-05-29'
+    },
+    {
+        category: '010',
+        title: 'Complete requested feature',
+        scheduledDate: '2024-06-08'
+    },
+    {
+        category: '009',
+        title: 'Clean the house',
+        scheduledDate: '2024-07-09'
+    },
+    {
+        category: '010',
+        title: 'Meet with mentor',
+        scheduledDate: '2024-07-15'
+    },
+    {
+        category: '003',
+        title: 'Work of freelance project',
+        scheduledDate: '2024-06-18'
+    },
+    {
+        category: '002',
+        title: 'Cook Dinner',
+        scheduledDate: '2024-06-15'
+    },
+    {
+        category: '008',
+        title: 'Teach coding',
+        scheduledDate: '2024-08-08'
+    },
+    {
+        category: '005',
+        title: 'Collaborate on an assignment',
+        scheduledDate: '2024-07-28'
+    },
+]
